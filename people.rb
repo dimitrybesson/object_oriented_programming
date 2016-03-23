1738923b2e00557abd1d78d77711cd3e7971cc43
Dimitry Besson <dimitrybesson@gmail.com>
2016-03-23 14:30:20 -0400

    Finished all of exercise 1

--- people.rb
+++ people.rb
@@ -6,7 +6,7 @@ class Person
   end
 
   def greeting
-    puts "Hi, my name is #{@name}" # alternatively, I could write self.name instead of @name
+    puts "Hi, my name is #{@name}." # alternatively, I could write self.name instead of @name
   end
 end
 
@@ -18,6 +18,20 @@ end
 
 class Instructor < Person
   def teach
-    puts "Everything in Ruby is an Object"
+    puts "Everything in Ruby is an Object."
   end
 end
+
+# Step 6
+chris = Instructor.new("Chris")
+chris.greeting
+
+# Step 7
+cristina = Student.new("Cristina")
+cristina.greeting
+
+# Step 8
+chris.teach
+cristina.learn
+#cristina.teach
+# This does not work because cristina is an instance of Student class, which does not have the #learn instance method.
