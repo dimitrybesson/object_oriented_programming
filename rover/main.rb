3704b1ff5df701eccd1cfef8c329ec10b376eaa4
Dimitry Besson <dimitrybesson@gmail.com>
2016-03-25 16:59:31 -0400

    Started planning.txt file and added mission control file

--- rover/main.rb
+++ rover/main.rb
@@ -6,30 +6,30 @@ require_relative 'plateau'
 # puts "Please provide plateau dimensions:"
 # plateau_initial = gets.chomp
 
-puts "Please provide a name for your rover:"
-rover_name = gets.chomp
-
-puts "Please provide initial location:"
-rover_initial = gets.chomp
-
-# Cutting up the initial
-initial = rover_initial.split(" ") # a list
-x_coordinate = initial[0].to_f
-y_coordinate = initial[1].to_f
-direction = initial[-1]
-
-rover1 = Rover.new(rover_name, x_coordinate, y_coordinate, direction)
-rover1.display
-
-puts "Please provide instruction:"
-rover_instruction = gets.chomp
-
-# Cutting up instruction
-instruction = rover_instruction.split("")
-rover1.read_instruction(instruction)
-
-puts rover1.rover_output
-rover1.display
+# puts "Please provide a name for your rover:"
+# rover_name = gets.chomp
+#
+# puts "Please provide initial location:"
+# rover_initial = gets.chomp
+#
+# # Cutting up the initial
+# initial = rover_initial.split(" ") # a list
+# x_coordinate = initial[0].to_f
+# y_coordinate = initial[1].to_f
+# direction = initial[-1]
+#
+# rover1 = Rover.new(rover_name, x_coordinate, y_coordinate, direction)
+# rover1.display
+#
+# puts "Please provide instruction:"
+# rover_instruction = gets.chomp
+#
+# # Cutting up instruction
+# instruction = rover_instruction.split("")
+# rover1.read_instruction(instruction)
+#
+# puts rover1.rover_output
+# rover1.display
 
 
 
--- rover/plateau.rb
+++ rover/plateau.rb
@@ -1,12 +1,26 @@
 class Plateau
-  #@array = []
-
+  attr_reader :rover_array
+  #@rover_array = ['hello']
   def initialize(x_max, y_max)
     @x_max = x_max
     @y_max = y_max
+    @rover_array = Array.new
   end
 
-  def communicate
+  def rover_array
+    @rover_array
+  end
+
+  def communicate(rover_direction)
+    puts "#{rover_direction}"
+    # The first user input initializes a plateau, maybe the rover should take note of this?
+    # set a location attribute?
+    # Alternatively,...
+  end
+
+  def create_rover(rover_id, rover_name, x_coordinate, y_coordinate, direction)
+    new_rover = Rover.new(rover_name, x_coordinate, y_coordinate, direction)
+    @rover_array << new_rover
 
   end
 end
