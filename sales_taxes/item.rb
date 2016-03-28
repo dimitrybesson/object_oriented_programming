8b8c1e66109cc0ec8d26d2b459169675184bb67e
Dimitry Besson <dimitrybesson@gmail.com>
2016-03-27 21:40:00 -0400

    Added display order, calculate tax, add items methods for order class.

---
+++ sales_taxes/item.rb
@@ -0,0 +1,49 @@
+class Item
+  attr_accessor :type, :price, :import, :quantity, :tax, :duty, :description
+
+  def initialize(type, price, import, quantity = 1, tax = "--", duty = "--", description = "")
+    @type = type
+    @price = price
+    @import = import
+    @quantity = quantity
+    @tax = tax # can we have this and duty start off 
+    @duty = duty
+    @description = description
+  end
+end
+
+class Order
+  attr_reader :item_list
+
+  def initialize
+    @item_list = Array.new
+  end
+
+  def display_order
+    puts "ITEM LIST:"
+    puts "TYPE:\tPRICE:\tIMPORT:\tQUANT:\tTAX:\tDUTY\tDESCRIPTION:"
+    @item_list.each do |item|
+      puts "#{item.type}\t#{item.price}\t#{item.import}\t#{item.quantity}\t#{item.tax}\t#{item.duty}\t#{item.description}"
+    end
+  end
+
+  def add_items(*items)
+    items.each do |item|
+      @item_list << item
+    end
+  end
+
+  def calculate_tax
+    @item_list.each do |item|
+      unless item.type == "book" || item.type == "medical" || item.type == "food"
+      item.tax = (item.price * 0.1).round(2)
+      end
+    end
+  end
+
+  def caculate_duty
+  end
+
+  def print_reciept
+  end
+end
---
+++ sales_taxes/test.rb
@@ -0,0 +1,6 @@
+(type, price, import, quantity = 1, description = "")
+
+apple = Item.new("food", 2.54, false)
+book = Item.new("book", 10.45, false)
+medicine = Item.new("medical", 23.43, true)
+perfume = Item.new("other", 65.11, true)
